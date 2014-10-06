e6c921173341a0b531631d3579e31d0ebb513e9d
Jasmin Bejtovic <jb223cp@student.lnu.se>
2014-10-06 23:05:43 +0200

    Redo för redovisning

---
+++ 1DV402.S2.L02A/1DV402.S2.L02A/AlarmClock.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L02A
+{
+    class AlarmClock
+    {
+        //private fields
+        private int _alarmHour;
+        private int _alarmMinute;
+        private int _hour;
+        private int _minute;
+
+        //Properties
+        public int AlarmHour
+        {
+            get
+            {
+                return _alarmHour;
+            }
+            set
+            {
+                if (value < 0 || 23 < value)
+                    throw new ArgumentException("Alarmtimmen är inte i intervallet 0-23");
+
+                _alarmHour = value;
+            }
+        }
+        public int AlarmMinute
+        {
+            get
+            {
+                return _alarmMinute;
+            }
+            set
+            {
+                if (value < 0 || 59 < value)
+                    throw new ArgumentException("Alarmminuten är inte i intervallet 0-59");
+
+                _alarmMinute = value;
+            }
+        }
+        public int Hour
+        {
+            get
+            {
+                return _hour;
+            }
+            set
+            {
+                if (value < 0 || 23 < value)
+                    throw new ArgumentException("Timmen är inte i intervallet 0-23");
+
+                _hour = value;
+            }
+        }
+        public int Minute
+        {
+            get
+            {
+                return _minute;
+            }
+            set
+            {
+                if (value < 0 || 59 < value)
+                    throw new ArgumentException("Minuten är inte i intervallet 0-59");
+
+                _minute = value;
+            }
+        }
+
+        /* Constructors chaining. First constructor without parameters
+         * calls the second constructor with two parameters, and the second
+         * constructor with two parameters calls the third constructor with four parameters.
+         * In the body of fourth constructor sets the values for fields.*/
+        public AlarmClock()
+            : this(0, 0)
+        {
+        }
+        public AlarmClock(int hour, int minute)
+            : this(hour, minute, 0, 0)
+        {
+        }
+        public AlarmClock(int hour, int minute, int alarmHour, int alarmMinute)
+        {
+            Hour = hour;
+            Minute = minute;
+            AlarmHour = alarmHour;
+            AlarmMinute = alarmMinute;
+        }
+        //Method TicToc
+        public bool TickTock()
+        {
+            //Checks if Minute is 59, and then reset it to 0
+            if (Minute == 59)
+            {
+                Minute = 0;
+
+                //Checks if Hour is 23, and then reset it to 0
+                switch (Hour)
+                {
+                    case 23: Hour = 0;
+                        break;
+                    default: Hour++;
+                        break;
+                }
+            }
+            else
+            {
+                Minute++;
+            }
+
+            if (Minute == AlarmMinute && Hour == AlarmHour)
+                return true;
+
+            return false;
+        }
+
+        //Method ToString
+
+        public override string ToString()
+        {
+            string row = Hour.ToString() + ":" + Minute.ToString("D2") + " (" + AlarmHour.ToString() + ":" + AlarmMinute.ToString("D2") + ")";
+            return row;
+        }
+    }
+}
---
+++ 1DV402.S2.L02A/1DV402.S2.L02A/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _1DV402.S2.L02A
+{
+    class Program
+    {
+        /* Field HorizontalLine that has to be static (the same as Main Method).
+         * Alternative is more complex so I could create an instance of Program class.
+         */
+        public static string HorizontalLine;
+        static void Main(string[] args)
+        {
+
+            HorizontalLine = " \n═════════════════════════════════════════════════════════════════════════════ ";
+
+            Console.Title = "Digital-Väckarklocka-Nivå A";
+            Console.BackgroundColor = ConsoleColor.DarkGreen;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(" ╔══════════════════════════════════════╗ ");
+            Console.WriteLine(" ║        Väckarklokan URLED <TM>       ║ ");
+            Console.WriteLine(" ║        modellnr.: 1DV402S2L2A        ║ ");
+            Console.WriteLine(" ╚══════════════════════════════════════╝ ");
+            Console.ResetColor();
+
+            //Test1-Default Constructor
+            ViewTestHeader(" Test 1.\n Test av standardkonstruktorn.\n");
+            AlarmClock acDefaultConstructor = new AlarmClock();
+            Console.WriteLine(acDefaultConstructor.ToString().PadLeft(15));
+
+            //Test2-Constructor with two parameters
+            ViewTestHeader(" Test 2.\n Test av konstruktorn med två parametrar, (9,42).\n");
+            AlarmClock acTwoParametersConstructor = new AlarmClock(9, 42);
+            Console.WriteLine(acTwoParametersConstructor.ToString().PadLeft(15));
+
+            //Test3-Constructor with four parameters
+            ViewTestHeader(" Test 3.\n Test av konstruktorn med fyra parametrar, (13,24,7,35).\n");
+            AlarmClock acFourParametersConstructor = new AlarmClock(13, 24, 7, 35);
+            Console.WriteLine(acFourParametersConstructor.ToString().PadLeft(15));
+
+            //Test4-TickTock() method with existing constructor
+            ViewTestHeader(" Test 4.\n Ställer befintligt AlarmClock-objekt till 23:58 och låter den gå 13 minuter.\n");
+            acFourParametersConstructor.Hour = 23;
+            acFourParametersConstructor.Minute = 58;
+            Run(acFourParametersConstructor, 13);
+
+            //Test5-TickTock() method with existing constructor and alarm time
+            ViewTestHeader(" Test 5.\n Ställer befintligt AlarmClock-objekt till tiden 6:12 och alarmtiden till 6:15 och låter den gå 6 minuter.\n");
+            acFourParametersConstructor.Hour = 6;
+            acFourParametersConstructor.Minute = 12;
+            acFourParametersConstructor.AlarmHour = 6;
+            acFourParametersConstructor.AlarmMinute = 15;
+            Run(acFourParametersConstructor, 6);
+
+            //Test6 Throwing exceptions - Properties
+            ViewTestHeader(" Test 6.\n Testar egenskaperna så att undantag kastas då tid och alarmtid tilldelas felaktiga värden.\n");
+            try
+            {
+                acFourParametersConstructor.Hour = -6;
+            }
+            catch (ArgumentException e)
+            {
+                ViewErrorMessage(e.Message);
+            }
+            try
+            {
+                acFourParametersConstructor.Minute = 62;
+            }
+            catch (ArgumentException e)
+            {
+                ViewErrorMessage(e.Message);
+            }
+            try
+            {
+                acFourParametersConstructor.AlarmHour = 30;
+            }
+            catch (ArgumentException e)
+            {
+                ViewErrorMessage(e.Message);
+            }
+            try
+            {
+                acFourParametersConstructor.AlarmMinute = -1;
+            }
+            catch (ArgumentException e)
+            {
+                ViewErrorMessage(e.Message);
+            }
+            //Test7 Throwing exceptions - Constructors
+            ViewTestHeader(" Test 7.\n Testar konstruktorerna så att undantag kastas då tid och alarmtid tilldelas felaktiga värden.\n");
+            try
+            {
+                AlarmClock constructorFail = new AlarmClock(-6, 62);
+            }
+            catch (ArgumentException e)
+            {
+                ViewErrorMessage(e.Message);
+            }
+            try
+            {
+                AlarmClock constructorFailAlarm = new AlarmClock(6, 51, 25, -2);
+            }
+            catch (ArgumentException e)
+            {
+                ViewErrorMessage(e.Message);
+            }
+
+        }
+        private static void Run(AlarmClock ac, int minutes)
+        {
+            for (int i = 0; i < minutes; i++)
+            {
+                if (ac.TickTock())
+                {
+                    Console.BackgroundColor = ConsoleColor.DarkBlue;
+                    Console.ForegroundColor = ConsoleColor.White;
+                    Console.WriteLine(" ♫  {0}  BEEP!  BEEP!  BEEP!  BEEP!", ac.ToString().PadLeft(11));
+                    Console.ResetColor();
+                }
+                else
+                {
+                    Console.WriteLine(ac.ToString().PadLeft(15));
+                }
+            }
+        }
+        private static void ViewErrorMessage(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+        private static void ViewTestHeader(string header)
+        {
+            Console.WriteLine(" " + HorizontalLine);
+            Console.WriteLine(header);
+        }
+    }
+}
